77938168d5d37c81f73a88a4d55e1bc75f40d653
Kristiyan Bonev <k.s.bonev@gmail.com>
2023-10-31 23:51:52 +0200

    More classes and objects

--- 5-6. Classes and Objects/Exercise/08_library/project/user.py
+++ 5-6. Classes and Objects/Exercise/08_library/project/user.py
@@ -1,8 +1,11 @@
+from typing import List
+
+
 class User:
     def __init__(self, user_id: int, username: str):
         self.user_id = user_id
         self.username = username
-        self.books = []
+        self.books: List[str] = []
 
     def info(self):
         return ', '.join(sorted(self.books))
