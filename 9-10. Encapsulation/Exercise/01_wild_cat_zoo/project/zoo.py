2227b84c1843e32eb31b6e421609152c474f47c5
Kristiyan Bonev <k.s.bonev@gmail.com>
2023-11-06 16:40:49 +0200

    Encapsulation exercise all done

---
+++ 9-10. Encapsulation/Exercise/01_wild_cat_zoo/project/zoo.py
@@ -0,0 +1,80 @@
+from project.caretaker import Caretaker
+from project.cheetah import Cheetah
+from project.keeper import Keeper
+from project.lion import Lion
+from project.tiger import Tiger
+from project.vet import Vet
+
+
+class Zoo:
+    def __init__(self, name: str, budget: int, animal_capacity: int, worker_capacity: int):
+        self.name = name
+        self.__budget = budget
+        self.__animal_capacity = animal_capacity
+        self.__workers_capacity = worker_capacity
+        self.animals = []
+        self.workers = []
+
+    def add_animal(self, animal: [Lion, Tiger, Cheetah], price: int):
+        if self.__animal_capacity <= 0:
+            return "Not enough space for animal"
+        elif self.__budget < price:
+            return "Not enough budget"
+        self.animals.append(animal)
+        self.__animal_capacity -= 1
+        self.__budget -= price
+        return f"{animal.name} the {type(animal).__name__} added to the zoo"
+
+    def hire_worker(self, worker: [Keeper, Caretaker, Vet]):
+        if self.__workers_capacity <= len(self.workers):
+            return "Not enough space for worker"
+        self.workers.append(worker)
+        return f"{worker.name} the {type(worker).__name__} hired successfully"
+
+    def fire_worker(self, worker_name: str):
+        for worker in self.workers:
+            if worker.name == worker_name:
+                self.workers.remove(worker)
+                return f"{worker_name} fired successfully"
+        return f"There is no {worker_name} in the zoo"
+
+    def pay_workers(self):
+        total_salaries = sum([worker.salary for worker in self.workers])
+        if self.__budget >= total_salaries:
+            self.__budget -= total_salaries
+            return f"You payed your workers. They are happy. Budget left: {self.__budget}"
+        return "You have no budget to pay your workers. They are unhappy"
+
+    def tend_animals(self):
+        animal_care_costs = sum([animal.money_for_care for animal in self.animals])
+        if self.__budget >= animal_care_costs:
+            self.__budget -= animal_care_costs
+            return f"You tended all the animals. They are happy. Budget left: {self.__budget}"
+        return "You have no budget to tend the animals. They are unhappy."
+
+    def profit(self, amount: int):
+        self.__budget += amount
+
+    def animals_status(self):
+        return (
+            f"You have {len(self.animals)} animals"
+            f"\n----- {self.class_type_counter(self.animals, Lion)} Lions:\n{self.class__repr__list(self.animals, Lion)}"
+            f"\n----- {self.class_type_counter(self.animals, Tiger)} Tigers:\n{self.class__repr__list(self.animals, Tiger)}"
+            f"\n----- {self.class_type_counter(self.animals, Cheetah)} Cheetahs:\n{self.class__repr__list(self.animals, Cheetah)}"
+        )
+
+    def workers_status(self):
+        return (
+            f"You have {len(self.workers)} workers"
+            f"\n----- {self.class_type_counter(self.workers, Keeper)} Keepers:\n{self.class__repr__list(self.workers, Keeper)}"
+            f"\n----- {self.class_type_counter(self.workers, Caretaker)} Caretakers:\n{self.class__repr__list(self.workers, Caretaker)}"
+            f"\n----- {self.class_type_counter(self.workers, Vet)} Vets:\n{self.class__repr__list(self.workers, Vet)}"
+        )
+
+    @staticmethod
+    def class_type_counter(classes_list: list, class_type: [Lion, Tiger, Cheetah, Keeper, Caretaker, Vet]):
+        return sum(1 for obj in classes_list if isinstance(obj, class_type))
+
+    @staticmethod
+    def class__repr__list(classes_list: list, class_type: [Lion, Tiger, Cheetah, Keeper, Caretaker, Vet]):
+        return "\n".join(obj.__repr__() for obj in classes_list if isinstance(obj, class_type))
---
+++ 9-10. Encapsulation/Exercise/04_restaurant/main.py
@@ -0,0 +1,33 @@
+from project.beverage.beverage import Beverage
+from project.food.cake import Cake
+from project.food.soup import Soup
+from project.product import Product
+
+
+def main():
+    # product = Product("coffee", 2.5)
+    # print(product.__class__.__name__)
+    # print(product.name)
+    # print(product.price)
+    # beverage = Beverage("coffee", 2.5, 50)
+    # print(beverage.__class__.__name__)
+    # print(beverage.__class__.__bases__[0].__name__)
+    # print(beverage.name)
+    # print(beverage.price)
+    # print(beverage.milliliters)
+    # soup = Soup("fish soup", 9.90, 230)
+    # print(soup.__class__.__name__)
+    # print(soup.__class__.__bases__[0].__name__)
+    # print(soup.name)
+    # print(soup.price)
+    # print(soup.grams)
+    cake = Cake("Triple Chocolate")
+    print(cake.__class__.__name__)
+    print(cake.name)
+    print(cake.price)
+    print(cake.calories)
+    print(cake.grams)
+
+
+if __name__ == '__main__':
+    main()
